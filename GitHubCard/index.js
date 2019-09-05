05e0e5955f05eb02d099b05f99ff09baff6c07e7
Dom <domeccleston@gmail.com>
2019-09-05 14:36:43 +0100

    improved follower get

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,74 +1,38 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
-
 let cardsContainer = document.querySelector('.cards');
 
 function getGithubData(user){
     axios.get(`https://api.github.com/users/${user}`)
     .then(response => {
-      cardsContainer.appendChild(cardCreator(response));
+      cardsContainer.appendChild(cardCreator(response.data));
     })
     .catch(error => console.log(error));
+
 }
 
+function getGithubFollowersData(user){
+  axios.get(`https://api.github.com/users/${user}/followers`)
+  .then(response => {
+    response.data.forEach((follower) => getGithubData(follower.login));
+  })
+  .catch(error => console.log(error));
 
+}
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
-
-   Skip to Step 3.
-*/
-
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
-
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
+getGithubFollowersData("domeccleston")
 
 const followersArray = ["domeccleston", "tetondan", "dustinmyers", "justsml", "luishrd", "bigknell", "ladrillo"];
-
+/* 
 followersArray.forEach((person) => getGithubData(person));
-
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
-
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
-
-*/
-
+ */
 function cardCreator(userData) {
 
-  console.log(userData.data);
+  console.log(userData);
 
   const cardDiv = document.createElement('div');
   cardDiv.classList.add("card");
 
   const userImg = document.createElement('img');
-  userImg.setAttribute("src", userData.data.avatar_url)
+  userImg.setAttribute("src", userData.avatar_url)
 
   const cardInfo = document.createElement('div');
   cardInfo.classList.add('card-info');
@@ -80,7 +44,7 @@ function cardCreator(userData) {
   const userLocation = document.createElement('p');
   const userProfile = document.createElement('p');
   const userProfileLink = document.createElement('a');
-  userProfileLink.setAttribute("href", userData.data.html_url)
+  userProfileLink.setAttribute("href", userData.html_url)
   const userFollowers = document.createElement('p');
   const userFollowing = document.createElement('p');
   const userBio = document.createElement('p');
@@ -91,31 +55,24 @@ function cardCreator(userData) {
   cardInfo.appendChild(userName);
   cardInfo.appendChild(userLocation);
   cardInfo.appendChild(userProfile);
-  userProfile.appendChild(userProfileLink);
   cardInfo.appendChild(userFollowers);
   cardInfo.appendChild(userFollowing);
   cardInfo.appendChild(userBio);
 
-  name.textContent = userData.data.name;
-  userName.textContent = userData.data.login;
-  userLocation.textContent = `Location: ${userData.data.location}`;
-  userProfile.textContent = `Profile:`
-  userProfileLink.textContent = userData.data.html_url;
-  userFollowers.textContent = `Followers: ${userData.data.followers}`;
-  userFollowing.textContent = `Following: ${userData.data.following}`;
-  userBio.textContent = userData.data.bio;
+
+  userProfile.appendChild(userProfileLink);
 
 
-  console.log(cardDiv);
+  name.textContent = userData.name;
+  userName.textContent = userData.login;
+  userLocation.textContent = userData.location === null ? "" : `Location: ${userData.location}`;
+  userProfile.textContent = `Profile: ${userProfileLink}`;
+  userProfileLink.textContent = userData.html_url;
+  userFollowers.textContent = `Followers: ${userData.followers}`;
+  userFollowing.textContent = `Following: ${userData.following}`;
+  userBio.textContent = userData.bio;
+
+
 
   return cardDiv;
 } 
-
-
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
